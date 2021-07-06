d26a7adb9a2fbe430a2e94791cd941f9304e0b3b
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-06 07:06:15 +0700

    change loop flow, removed redundant check_player_health

--- main.rb
+++ main.rb
@@ -2,15 +2,17 @@ require_relative './lib/CharacterFactory'
 require_relative './lib/Game'
 
 def start_game
-  char1 = create_char('Jin Sakai', 200, 100)
-  char1.to_ss
-  char2 = create_char('Genghis Khan', 300, 50)
+  char1 = create_char('Jin Sakai', 200, 50, true)
+  char1.to_s
+  char2 = create_char('Genghis Khan', 500, 50)
   char2.to_s
   game = Game.new(char1, char2)
-  until game.is_finished
+  loop do
     game.player_attacking(game.characters[0], game.characters[1])
+    break if game.is_finished
+
     game.player_attacking(game.characters[1], game.characters[0])
-    game.check_player_health
+    break if game.is_finished
   end
 end
 
