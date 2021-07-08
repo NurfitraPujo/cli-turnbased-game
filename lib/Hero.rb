16149238ae555e2363eca164ef7de1e506cd84e9
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-08 19:53:21 +0700

    finished create spec for Hero

--- lib/Hero.rb
+++ lib/Hero.rb
@@ -1,22 +1,26 @@
 require_relative './Character'
 
 class Hero < Character
-  def deflect_attack?
-    deflect_chance = rand(1..5)
-    deflect_chance < 5
+  def deflect_probability
+    rand(1..5)
+  end
+
+  def deflect_attack?(probability = deflect_probability)
+    probability < 5
   end
 
   def heal(ally)
-    print_healing_event(ally)
+    puts print_healing_event(ally)
     ally.hp += 20
   end
 
   def print_healing_event(ally)
-    puts "#{@name} heals #{ally.name}, restoring 20 points"
+     "#{@name} heals #{ally.name}, restoring 20 points"
   end
 
-  def receive_damage(damage)
-    if deflect_attack?
+  def receive_damage(damage, assigned_deflect_probability = nil)
+    probability = assigned_deflect_probability || deflect_probability
+    if deflect_attack?(probability)
       puts "#{@name} deflected the attack"
     else
       super(damage)
--- spec/hero_spec.rb
+++ spec/hero_spec.rb
@@ -8,6 +8,32 @@ describe Hero do
 
   describe '#deflect_attack?' do
     it 'should return true if the deflect_chance is between 1..4 and return false if deflect_chance is 5' do
+      deflect_chance = @hero.deflect_probability
+      expected = deflect_chance < 5
+      expect(@hero.deflect_attack?(deflect_chance)).to eq(expected)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'should not substract hero hp if he deflect the attack' do
+      deflect_chance = @hero.deflect_probability
+      expected = deflect_chance < 5 ? @hero.hp : @hero.hp - 20
+      @hero.receive_damage(20, deflect_chance)
+      expect(@hero.hp).to eq(expected)
+    end
+  end
+
+  describe '#hero_heals_ally' do
+    before do
+      @ally = Character.new('Ally test', 30, 20)
+    end
+    it 'should print valid healing event based on its args' do
+      expected = "#{@hero.name} heals #{@ally.name}, restoring 20 points"
+      expect(@hero.print_healing_event(@ally)).to eq(expected)
+    end
+    it 'allies that healed had their hp restored by 20 points' do
+      expected = @ally.hp + 20
+      expect(@hero.heal(@ally)).to eq(expected)
     end
   end
 end
