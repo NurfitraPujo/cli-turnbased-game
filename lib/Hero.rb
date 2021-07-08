ea7c130955e94254c2be7d79cc40580eec86899b
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-08 15:19:47 +0700

    Add capabilites to heal allies on Hero

--- lib/Hero.rb
+++ lib/Hero.rb
@@ -1,10 +1,18 @@
 require_relative './Character'
 
 class Hero < Character
-
   def deflect_attack?
     deflect_chance = rand(1..5)
     deflect_chance < 5
+  end
+
+  def heal_allies(ally)
+    print_healing_event(ally)
+    ally.hp += 20
+  end
+
+  def print_healing_event(ally)
+    puts "#{@name} heals #{ally.name}, restoring 20 points"
   end
 
   def receive_damage(damage)
